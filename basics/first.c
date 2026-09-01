67efd5738e2466794a5edf16838e59ad22c27f29
AzizTUN <azizabdallah495@gmail.com>
2026-09-01 19:39:52 +0100

    first_C

---
+++ basics/first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int x ;
+    x = 7 ;
+    float y = 3.22 ;
+    int z = 77 ;
+    double k = 3254.325 ;
+    char msg[] = "bruh" ;
+    bool ok = true;
+    printf("Hello, CPI! u have %d years and %d numbers with %.2f gpa with %.3lf ur a %s ARE U OK : %d!\n",z,x,y,k,msg,ok);
+
+    return 0;
+}
